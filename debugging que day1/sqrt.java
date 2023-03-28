6ab96ef2016ad3db67fbcb28ff8833c699b05e31
axaish <112846324+axaish@users.noreply.github.com>
2023-03-28 23:08:51 +0530

    Add files via upload

---
+++ debugging que day1/sqrt.java
@@ -0,0 +1,20 @@
+class Solutionsqrt
+{
+static int mySqrt(int x)
+{
+if (x == 0 || x == 1)
+return x;
+int i = 1, result = 1;
+while (result <= x)
+{
+i++;
+result = i * i;
+}
+return i - 1;
+}
+public static void main(String[] args)
+{
+int x = 11;
+System.out.print(mySqrt(x));
+}
+}
